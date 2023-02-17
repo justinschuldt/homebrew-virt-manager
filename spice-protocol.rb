eedbfa0813b91f682a45cc78b8e38ce26243687e
Fernando Verdugo <fv3rdugo@gmail.com>
2023-02-17 11:40:29 +0100

    Update spice-protocol.rb

--- spice-protocol.rb
+++ spice-protocol.rb
@@ -1,8 +1,8 @@
 class SpiceProtocol < Formula
   desc "Headers for SPICE protocol"
   homepage "https://www.spice-space.org/"
-  url "https://www.spice-space.org/download/releases/spice-protocol-0.12.13.tar.bz2"
-  sha256 "89ee11b202d2268e061788e6ace114e1ff18c7620ae64d1ca3aba252ee7c9933"
+  url "https://www.spice-space.org/download/releases/spice-protocol-0.14.1.tar.bz2"
+  sha256 "79e6da61834b080a143234c1cd4c099a8ead1a64b5039489610b72ab282c132a"
 
   def install
     system "./configure", "--disable-silent-rules",
